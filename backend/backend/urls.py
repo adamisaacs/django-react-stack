27259068f54ea5926598d8a5a05d7e21c3da0741
Adam Isaacs <adamisaacs@gmail.com>
2023-01-31 07:48:01 +0000

    setup better URLs

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,15 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.shortcuts import render
 
 
 def render_react(request):
-    return render(request, 'index.html')
-    
+    return render(request, "index.html")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', render_react),
+    re_path(r"^apps/.*$", render_react),
+    re_path(r"^$", render_react), ## Use React for empty URLs
+    #re_path(r"^(?:.*)/?$", render_react), # Use React for every URL
 ]
